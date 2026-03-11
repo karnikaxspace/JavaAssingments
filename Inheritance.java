458a187edda9111514c10b03cbf4e0757cd3491c
karnikaxspace <karnikaparashar5@gmail.com>
2026-03-11 22:39:17 +0530

    Update Inheritance.java
    Added classes for Animal and Dog to demonstrate inheritance.

--- Inheritance.java
+++ Inheritance.java
@@ -1,3 +1,23 @@
-public class Inheritance {
-    
+// Parent class
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+// Child class
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+// Main class
+public class Inheritance{
+    public static void main(String[] args) {
+        Dog d = new Dog();   
+
+        d.eat();   
+        d.bark(); 
+    }
 }
