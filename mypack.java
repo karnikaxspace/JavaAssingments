dcdf048fd77d5566225cbdc1309e43cfeacc64b3
karnikaxspace <karnikaparashar5@gmail.com>
2026-04-24 21:44:21 +0530

    Create mypack.java

---
+++ mypack.java
@@ -0,0 +1,8 @@
+// Q17. package mypack;
+package mypack;
+
+public class A { public void show(){ System.out.println("A"); } }
+class B { public void show(){ System.out.println("B"); } }
+class C { public void show(){ System.out.println("C"); } }
+class D { public void show(){ System.out.println("D"); } }
+class E { public void show(){ System.out.println("E"); } }
